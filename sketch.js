e4e1288be4626846d529036dedc26e3856407a8f
coder123-project <83872425+coder123-project@users.noreply.github.com>
2022-08-18 23:52:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var gameState = "Play";
+var ground, groundImage, bg, bgImage;
+var mario, mAnimation;
+var obstacle, obstacleAnimation, coin, coinsImage;
+var score;
+var obstaclesGroup, coinsGroup;
+var jumpSound, dieSound, checkPointSound;
+var collidedAnimation;
+var gameOver, gameOverImage, restart, restartImage;
+var highScore=0;
+
+
+function preload() {
+
+
+
+}
+
+function setup() {
+  createCanvas(600, 400);
+
+
+}
+
+function draw() {
+  background(210);
+
+
+}
+
